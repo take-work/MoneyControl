03cf3320dfdaf21676689f1dc2a621afcc4cf7f8
take-work <ts.lworkspace@gmail.com>
2016-03-27 21:59:00 +0900

    入力画面を作成した。

--- MoneyInput.php
+++ MoneyInput.php
@@ -1,1 +1,40 @@
-<?php
+<html>
+  <head>
+    <meta charset="UTF-8">
+  </head>
+  <body>
+    <p>登録する情報を記入してください。</p>
+    <form id="formInput" name="formInput" method="post" action="input_do.php">
+	  <dl>
+		<dt>
+		  <label for="UserName">名前</label>
+		</dt>
+		<dd>
+		  <input name="UserName" type="text" id="UserName" size="20" maxlength="255" />
+		</dd><br />
+
+		<dt>
+		  <label for="IventName">イベント名</label>
+		</dt>
+		<dd>
+		  <input name="IventName" type="text" id="IventName" size="20" maxlength="255" />
+		</dd><br />
+
+		<dt>
+		  <label for="Day">日付</label>
+		</dt>
+		<dd>
+		  <input name="Day" type="text" id="Day" size="10" maxlength="255" />
+		</dd><br />
+
+		<dt>
+		  <label for="price">集計金額</label>
+		</dt>
+		<dd>
+		  <input name="price" type="text" id="price" size="10" maxlength="10" />円
+		</dd>
+      </dl><br />
+	  <input type="submit" value="登録する" />
+	</form>
+  </body>
+</html>
