064bc4fd8aae1f19e676621597480d9faedf01b4
take-work <ts.lworkspace@gmail.com>
2016-03-31 12:05:03 +0900

    入力日をDBに登録できるようにした。

--- Complete/MoneyComplete.php
+++ Complete/MoneyComplete.php
@@ -10,10 +10,13 @@ $db = mysqli_connect('localhost', 'root', 'i1090149', 'MoneyControl') or die(mys
 
 mysqli_set_charset($db, 'UTF8');
 
+$inputDay = $_POST['Year']."-".$_POST['Month']."-".$_POST['Day'];
+
 $sql = sprintf(
-	'INSERT INTO Calculation SET UserName="%s", EventName="%s", Price=%d',
+	'INSERT INTO Calculation SET UserName="%s", EventName="%s", InputDay="%s", Price=%d',
 	mysqli_real_escape_string($db, $_POST['UserName']),
 	mysqli_real_escape_string($db, $_POST['EventName']),
+	mysqli_real_escape_string($db, $inputDay),
 	mysqli_real_escape_string($db, $_POST['Price'])
 );
 
