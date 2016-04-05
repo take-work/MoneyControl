c6ba0dc6936488a385875f05b8a01c04f6ea09e8
take-work <ts.lworkspace@gmail.com>
2016-04-05 22:56:38 +0900

    イベント一覧のテーブルを見易くした。

--- Lists/EventList.php
+++ Lists/EventList.php
@@ -3,7 +3,7 @@
 	<title>イベント一覧</title>
 </head>
 <body>
-<font size="4">イベント一覧</font><br><br><br>
+  <font size="4">イベント一覧</font><br><br><br>
 
   <?php
     header("Content-Type: text/html; charset=UTF-8");
@@ -15,8 +15,8 @@
     mysqli_set_charset($db, 'UTF8');
 
     $recordSet = mysqli_query($db, 'SELECT * FROM Events');
-    while ($data = mysqli_fetch_assoc($recordSet)) {
   ?>
+
   <table width="1000" border="1" cellspacing="1" cellpadding="8">
     <tbody>
       <tr>
@@ -25,16 +25,27 @@
         <th>開始日</th>
         <th>終了日</th>
         <th>準備費用</th>
-        <th>参加スタッフ数</th>
-        <th>参加サークル数</th>
+        <th>スタッフ数</th>
+        <th>サークル数</th>
+        <th>データ更新</th>
+        <th>データ削除</th>
+        <th>金額情報管理</th>
       </tr>
-      <td><?=htmlspecialchars($data['event_name'])?></td>
-      <td><?=htmlspecialchars($data['host'])?></td>
-      <td align="center"><?=htmlspecialchars($data['start_day'])?></td>
-      <td align="center"><?=htmlspecialchars($data['end_day'])?></td>
-      <td align="center"><?=htmlspecialchars($data['price'])?></td>
-      <td align="center"><?=htmlspecialchars($data['staff'])?></td>
-      <td align="center"><?=htmlspecialchars($data['circle'])?></td>
+  <?php
+    while ($data = mysqli_fetch_assoc($recordSet)) {
+  ?>
+      <tr>
+        <td><?=htmlspecialchars($data['event_name'])?></td>
+        <td><?=htmlspecialchars($data['host'])?></td>
+        <td align="center"><?=htmlspecialchars($data['start_day'])?></td>
+        <td align="center"><?=htmlspecialchars($data['end_day'])?></td>
+        <td align="center"><?=htmlspecialchars($data['price'])?></td>
+        <td align="center"><?=htmlspecialchars($data['staff'])?></td>
+        <td align="center"><?=htmlspecialchars($data['circle'])?></td>
+        <td align="center">未作成</td>
+        <td align="center">未作成</td>
+        <td align="center">未作成</td>
+      </tr>
   <?php
     }
   ?>
