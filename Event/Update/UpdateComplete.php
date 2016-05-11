161c256f15352dc496dcd190bd5661b1558763e0
take-work <ts.lworkspace@gmail.com>
2016-05-11 16:22:37 +0900

    ファイルのパスが間違っていたので修正した。

--- Event/Update/UpdateComplete.php
+++ Event/Update/UpdateComplete.php
@@ -33,9 +33,9 @@
 
   <br>
   <a href="../EventInput.php">イベントの新規登録</a><br>
-  <a href="../Lists/EventList.php">登録されているイベント</a><br>
-  <a href="../MoneyInput.php">金額情報の入力</a><br>
-  <a href="../Lists/TotalMoney.php">現在の金額状況</a><br>
+  <a href="../EventList.php">登録されているイベント</a><br>
+  <a href="../../../Money/MoneyInput.php">金額情報の入力</a><br>
+  <a href="../../../Money/TotalMoney.php">現在の金額状況</a><br>
 
 </body>
 </html>
