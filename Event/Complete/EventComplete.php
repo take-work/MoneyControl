933ead27f4620f13e98d06454263d0d66d92024f
take-work <ts.lworkspace@gmail.com>
2016-05-11 12:57:26 +0900

    ファイルのパスが間違っていたので修正した。

--- Event/Complete/EventComplete.php
+++ Event/Complete/EventComplete.php
@@ -7,7 +7,7 @@
   <?php
     header("Content-Type: text/html; charset=UTF-8");
 
-    require_once(dirname(__FILE__).'../../database.php');
+    require_once(dirname(__FILE__).'../../../database.php');
 
     $MyDB = new MyDB;
     $db = $MyDB->dbConnect();
@@ -33,10 +33,12 @@
     echo "登録しました。";
   ?>
 
+  <br>
+  <br>
   <a href="../EventInput.php">イベントの新規登録</a><br>
-  <a href="../EventList.php">登録されているイベント</a>
+  <a href="../EventList.php">登録されているイベント</a><br>
   <a href="../../Money/MoneyInput.php">金額情報の入力</a><br>
-  <a href="../../Money/TotalMoney.php">現在の金額状況</a><br>
+  <a href="../../Money/TotalMoney.php">現在の金額状況</a>
 
 </body>
 </html>
