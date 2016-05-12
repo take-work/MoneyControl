6ad04cebb9bc9e15a27b639b66f44fa6f2339f17
take-work <ts.lworkspace@gmail.com>
2016-05-12 12:15:20 +0900

    サークル情報を削除できるようにした。(Issue #6)

---
+++ Event/Circle/Delete/deleteComplete.php
@@ -0,0 +1,34 @@
+<html>
+<head>
+	<title>サークルデータ削除完了画面</title>
+</head>
+<body>
+
+  <?php
+    header("Content-Type: text/html; charset=UTF-8");
+
+    require_once(dirname(__FILE__).'../../../../database.php');
+
+    $MyDB = new MyDB;
+    $db = $MyDB->dbConnect();
+
+    mysqli_set_charset($db, 'UTF8');
+
+    $sql = sprintf(
+      'DELETE FROM Circles WHERE id = %d',
+        mysqli_real_escape_string($db, $_POST['id'])
+    );
+
+    mysqli_query($db, $sql) or die(mysqli_error($db));
+
+    echo "データの削除が完了しました。";
+  ?>
+
+  <br><br>
+  <a href="../EventInput.php">イベントの新規登録</a><br>
+  <a href="../../EventList.php">登録されているイベント</a><br>
+  <a href="../../../Money/MoneyInput.php">金額情報の入力</a><br>
+  <a href="../../../Money/TotalMoney.php">現在の金額状況</a>
+
+</body>
+</html>
--- Event/Circle/circleList.php
+++ Event/Circle/circleList.php
@@ -5,11 +5,15 @@
 <body>
   <font size="4">サークル一覧</font><br><br>
 
-  <a href="circleInput.php"><input type="submit" name="createCircle" value="新しいサークルを作成する"></a>
+  <form id="createCircle" name="createCircle" method="post" action="circleInput.php?actin=create&id=<?=htmlspecialchars($_GET['id'])?>">
+    <input type="hidden" name="id" value="<?=htmlspecialchars($_GET['id'])?>"></input>
+    <input type="submit" name="createCircle" value="新しいサークルを作成する">
+  </form>
   <?php
     header("Content-Type: text/html; charset=UTF-8");
-    require_once(dirname(__FILE__).'../../database.php');
+    require_once(dirname(__FILE__).'../../../database.php');
 
+    $id = $_GET['id'];
     $MyDB = new MyDB;
     $db = $MyDB->dbConnect();
 
@@ -21,21 +25,21 @@
   <table width="1200" border="1" cellspacing="1" cellpadding="8">
     <tbody>
       <tr>
-        <th>参加イベント</th>
         <th>サークル名</th>
+        <th>代表者</th>
         <th>売上</th>
+        <th>データの編集</th>
         <th>データの削除</th>
       </tr>
   <?php
     while ($data = mysqli_fetch_assoc($recordSet)) {
   ?>
       <tr>
-        <td><?=htmlspecialchars($data['event_name'])?></td>
         <td align="center"><?=htmlspecialchars($data['circle_name'])?></td>
         <td><?=htmlspecialchars($data['host'])?></td>
         <td align="center"><?=htmlspecialchars($data['proceeds'])?></td>
         <td align="center"><a href="Update/dataUpdate.php?actin=update&id=<?=htmlspecialchars($data['id'])?>">編集ページ</a></td>
-        <td align="center"><a href="Delete/deleteConfirm.php?actin=delete&id=<?=htmlspecialchars($data['id'])?>"><input type="submit" value="削除"></a></td>
+        <td align="center"><a href="Delete/deleteComfirm.php?actin=delete&id=<?=htmlspecialchars($data['id'])?>"><input type="submit" value="削除"></a></td>
       </tr>
   <?php
     }
