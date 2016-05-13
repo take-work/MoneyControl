d3e91bdef1a0ceb6e0a674d6eed90dc14290ef73
take-work <ts.lworkspace@gmail.com>
2016-05-13 12:37:38 +0900

    イベント登録・サークル登録ページをテーブルにして分かりやすくした。(Issue #14)

--- Circle/Create/circleInput.php
+++ Circle/Create/circleInput.php
@@ -5,35 +5,42 @@
   </head>
   <body>
     <p>登録するサークル情報を記入してください。</p>
-    <form id="formInput" name="formInput" method="post" action="circleComplete.php">
-      <dl>
-        <dt>
-		  <label for="circleName">サークル名</label>
-		</dt>
-		<dd>
-		  <input name="circleName" type="text" id="circleName" maxlength="255" />
-		</dd><br />
 
-		<dt>
-		  <label for="host">主催者</label>
-		</dt>
-		<dd>
-		  <input name="host" type="text" id="host" maxlength="255" />
-		</dd><br />
+    <table width="800" border="1" cellspacing="1" cellpadding="8">
+      <tbody>
+        <tr>
+          <th>サークル名</th>
+          <th>代表者</th>
+          <th>売上</th>
+          <th>データの登録</th>
+        </tr>
 
-		<dt>
-		  <label for="proceeds">売上金(目標or最終)</label>
-		</dt>
-		<dd>
-		  <input name="proceeds" type="text" id="proceeds" maxlength="10" />円
-		</dd>
-      </dl><br />
+        <form id="formInput" name="formInput" method="post" action="circleComplete.php">
+          <tr>
+	   	    <td align="center">
+		      <input name="circleName" type="text" id="circleName" maxlength="255" />
+		    </td>
 
-      <input type="hidden" name="id" value="<?=htmlspecialchars($_GET['id'])?>"></input>
-	  <input type="submit" value="登録する" /><br>
-	</form>
+		    <td align="center">
+		      <input name="host" type="text" id="host" maxlength="255" />
+		    </td>
 
-    <a href="../circleList">サークル一覧に戻る</a><br>
+		    <td align="center">
+		      <input name="proceeds" type="text" id="proceeds" maxlength="10" />円
+		    </td>
+
+            <td align="center">
+              <input type="hidden" name="id" value="<?=htmlspecialchars($_GET['id'])?>"></input>
+  	          <input type="submit" value="登録する" />
+  	        </td>
+  	      </tr>
+	    </form>
+
+	  </tbody>
+	</table>
+
+    <br>
+    <a href="../circleList?actin=confirm&id=<?=htmlspecialchars($_GET['id'])?>">サークル一覧に戻る</a><br>
     <a href="../../Event/EventList.php">イベント一覧に戻る</a>
   </body>
 </html>
--- Event/Create/EventInput.php
+++ Event/Create/EventInput.php
@@ -5,59 +5,59 @@
   </head>
   <body>
     <p>登録するイベント情報を記入してください。</p>
-    <form id="formInput" name="formInput" method="post" action="EventComplete.php">
-	  <dl>
-		<dt>
-		  <label for="eventName">イベント名</label>
-		</dt>
-		<dd>
-		  <input name="eventName" type="text" id="eventName" size="20" maxlength="255" />
-		</dd><br />
 
-		<dt>
-		  <label for="host">主催者</label>
-		</dt>
-		<dd>
-		  <input name="host" type="text" id="host" size="20" maxlength="255" />
-		</dd><br />
+    <table width="1200" border="1" cellspacing="1" cellpadding="8">
+      <tbody>
+        <tr>
+          <th>イベント名</th>
+          <th>主催者</th>
+          <th>開始年月日</th>
+          <th>終了年月日</th>
+          <th>準備費用</th>
+          <th>スタッフ数</th>
+          <th>データの登録</th>
+        </tr>
 
-		<dt>
-		  <label for="date">開始日</label>
-		</dt>
-		<dd>
-		  西暦<input name="startYear" type="text" id="startYear" size="8" maxlength="10" />年 &nbsp;
-		  <input name="startMonth" type="text" id="startMonth" size="4" maxlength="4" />月 &nbsp;
-		  <input name="startDay" type="text" id="startDay" size="4" maxlength="4" />日
-		</dd><br />
+        <form id="formInput" name="formInput" method="post" action="EventComplete.php">
+	      <tr>
+		    <td align="center">
+		      <input name="eventName" type="text" id="eventName" size="20" maxlength="255" />
+		    </td>
 
-		<dt>
-		  <label for="Date">終了日</label>
-		</dt>
-		<dd>
-		  西暦<input name="endYear" type="text" id="endYear" size="8" maxlength="10" />年 &nbsp;
-		  <input name="endMonth" type="text" id="endMonth" size="4" maxlength="4" />月 &nbsp;
-		  <input name="endDay" type="text" id="endDay" size="4" maxlength="4" />日
-		</dd><br />
+ 		    <td align="center">
+		      <input name="host" type="text" id="host" size="20" maxlength="255" />
+		    </td>
 
-		<dt>
-		  <label for="price">準備費用</label>
-		</dt>
-		<dd>
-		  <input name="price" type="text" id="price" size="10" maxlength="10" />円
-		</dd><br />
+		    <td align="center">
+		      西暦<input name="startYear" type="text" id="startYear" size="8" maxlength="10" />年 &nbsp;
+		      <input name="startMonth" type="text" id="startMonth" size="4" maxlength="4" />月 &nbsp;
+		      <input name="startDay" type="text" id="startDay" size="4" maxlength="4" />日
+		    </td>
 
-		<dt>
-		  <label for="staff">スタッフ数</label>
-		</dt>
-		<dd>
-		  <input name="staff" type="text" id="staff" size="5" maxlength="10" />人
-		</dd><br />
+		    <td align="center">
+		      西暦<input name="endYear" type="text" id="endYear" size="8" maxlength="10" />年 &nbsp;
+		      <input name="endMonth" type="text" id="endMonth" size="4" maxlength="4" />月 &nbsp;
+		      <input name="endDay" type="text" id="endDay" size="4" maxlength="4" />日
+		    </td>
 
-      </dl><br />
+		    <td align="center">
+		      <input name="price" type="text" id="price" size="10" maxlength="10" />円
+		    </td>
 
-	  <input type="submit" value="登録する" /><br>
-	</form>
+		    <td align="center">
+		      <input name="staff" type="text" id="staff" size="5" maxlength="10" />人
+		    </td>
 
-    <a href="../EventList.php">登録されているイベント</a>
+            <td align="center">
+       	      <input type="submit" value="登録する" />
+       	    </td>
+       	  </tr>
+
+	    </form>
+	  </tbody>
+	</table>
+
+    <br>
+    <a href="../EventList.php">イベント一覧に戻る</a>
   </body>
 </html>
